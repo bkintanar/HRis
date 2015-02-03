231dbcfbceba682acf7e4a63e87f9ecead487a3e
g3e729 <mr.jimcallanta@gmail.com>
2015-02-04 06:51:11 +0800

    Modified JobController.php

--- app/Http/Controllers/Profile/JobController.php
+++ app/Http/Controllers/Profile/JobController.php
@@ -38,7 +38,6 @@ class JobController extends Controller {
 
         $this->data['employee'] = $employee;
         $this->data['job_histories'] = $employee->orderedJobHistories();
-        $this->data['number_job_histories'] = count($employee->orderedJobHistories());
 
         $this->data['disabled'] = 'disabled';
         $this->data['pim'] = $request->is('*pim/*') ? true : false;
@@ -68,7 +67,6 @@ class JobController extends Controller {
 
         $this->data['employee'] = $employee;
         $this->data['job_histories'] = $employee->orderedJobHistories();
-        $this->data['number_job_histories'] = count($employee->orderedJobHistories());
 
         $this->data['disabled'] = '';
         $this->data['pim'] = $request->is('*pim/*') ? true : false;
