99bfa372c4be748e15c702e553207afa4538685b
Jim Callanta <mr.jimcallanta@gmail.com>
2015-02-13 00:57:41 +0800

    Salary Computation Updates

--- database/seeds/WorkShiftsTableSeeder.php
+++ database/seeds/WorkShiftsTableSeeder.php
@@ -18,23 +18,51 @@ class WorkShiftsTableSeeder extends Seeder {
             [
                 [
                     'id'        => 1,
-                    'name'      => 'Morning Shift',
-                    'from_time' => '07:00:00',
-                    'to_time'   => '16:00:00',
+                    'name'      => 'Admin',
+                    'from_time' => '06:00:00',
+                    'to_time'   => '17:00:00',
                     'duration'  => 9
                 ],
                 [
                     'id'        => 2,
-                    'name'      => 'Afternoon Shift',
+                    'name'      => '08:00 am - 05:00',
+                    'from_time' => '08:00:00',
+                    'to_time'   => '17:00:00',
+                    'duration'  => 9
+                ],
+                [
+                    'id'        => 3,
+                    'name'      => '02:00 pm - 11:00 pm',
                     'from_time' => '14:00:00',
                     'to_time'   => '23:00:00',
                     'duration'  => 9
                 ],
                 [
-                    'id'        => 3,
-                    'name'      => 'Night Shift',
+                    'id'        => 4,
+                    'name'      => '08:00 pm - 05:00 am',
+                    'from_time' => '20:00:00',
+                    'to_time'   => '05:00:00',
+                    'duration'  => 9
+                ],
+                [
+                    'id'        => 5,
+                    'name'      => '11:00 pm - 08:00 am',
                     'from_time' => '23:00:00',
                     'to_time'   => '08:00:00',
+                    'duration'  => 9
+                ],
+                [
+                    'id'        => 6,
+                    'name'      => '05:00 pm - 02:00 am',
+                    'from_time' => '17:00:00',
+                    'to_time'   => '02:00:00',
+                    'duration'  => 9
+                ],
+                [
+                    'id'        => 7,
+                    'name'      => '06:00 pm - 03:00 am',
+                    'from_time' => '18:00:00',
+                    'to_time'   => '03:00:00',
                     'duration'  => 9
                 ],
 
