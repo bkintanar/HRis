756dd0681cd740ddc66d70a3f23b7b2bfb56c7f5
Harlequin Doyon <harlequin.doyon@gmail.com>
2015-08-25 15:43:03 +0800

    Added the attendance controller and change the navlinks table seeder

---
+++ app/Http/Controllers/Time/Attendance/AttendanceController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Http\Controllers\Time\Attendance;
+
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
+use HRis\Http\Controllers\Controller;
+use HRis\Http\Requests;
+use Illuminate\Http\Request;
+
+class AttendanceController extends Controller
+{
+    /**
+     * Constructor
+     * @param Sentinel $auth
+     * @author Harlequin Doyon
+     */
+    public function __construct(Sentinel $auth)
+    {
+        parent::__construct($auth);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
