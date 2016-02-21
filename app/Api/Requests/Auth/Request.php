945750b3d162e1d79b9b8722f6a11570f8a60849
Bertrand Kintanar <bertrand.kintanar@gmail.com>
2016-02-21 23:27:52 +0800

    Added FormRequest Validation

---
+++ app/Api/Requests/Auth/Request.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the HRis Software package.
+ *
+ * HRis - Human Resource and Payroll System
+ *
+ * @link    http://github.com/HB-Co/HRis
+ */
+namespace HRis\Api\Requests\Auth;
+
+use HRis\Http\Requests\Request as BaseRequest;
+
+class Request extends BaseRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     *
+     * @author Bertrand Kintanar <bertrand.kintanar@gmail.com>
+     */
+    public function rules()
+    {
+        return [
+            'email'    => 'required|exists:users,email',
+            'password' => 'required'
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     *
+     * @author Bertrand Kintanar <bertrand.kintanar@gmail.com>
+     */
+    public function authorize()
+    {
+        return true;
+    }
+}
