75d735d58daef10da6b97b19647c631bfd2cfc5a
Bertrand Kintanar <bertrand.kintanar@gmail.com>
2016-04-16 18:25:40 +0800

    Updated swagger-ui

--- resources/views/vendor/swaggervel/index.blade.php
+++ resources/views/vendor/swaggervel/index.blade.php
@@ -1,14 +1,20 @@
+<?php
+header('Access-Control-Allow-Origin: *');
 
-<!DOCTYPE html>
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+?>
+        <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Swagger UI</title>
-    <link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" />
+    <title>HRis API Documentation</title>
+    <link rel="icon" type="image/png" href="vendor/swaggervel/images/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="vendor/swaggervel/images/favicon-16x16.png" sizes="16x16" />
     <link href='vendor/swaggervel/css/typography.css' media='screen' rel='stylesheet' type='text/css'/>
     <link href='vendor/swaggervel/css/reset.css' media='screen' rel='stylesheet' type='text/css'/>
-    <link href='vendor/swaggervel/css/screen.css' media='screen' rel='stylesheet' type='text/css'/>
+    <link href='vendor/swaggervel/css/themes/muted.css' rel='stylesheet' type='text/css'/>
     <link href='vendor/swaggervel/css/reset.css' media='print' rel='stylesheet' type='text/css'/>
     <link href='vendor/swaggervel/css/print.css' media='print' rel='stylesheet' type='text/css'/>
     <script src='vendor/swaggervel/lib/jquery-1.8.0.min.js' type='text/javascript'></script>
@@ -16,13 +22,12 @@
     <script src='vendor/swaggervel/lib/jquery.wiggle.min.js' type='text/javascript'></script>
     <script src='vendor/swaggervel/lib/jquery.ba-bbq.min.js' type='text/javascript'></script>
     <script src='vendor/swaggervel/lib/handlebars-2.0.0.js' type='text/javascript'></script>
-    <script src='vendor/swaggervel/lib/js-yaml.min.js' type='text/javascript'></script>
-    <script src='vendor/swaggervel/lib/lodash.min.js' type='text/javascript'></script>
+    <script src='vendor/swaggervel/lib/underscore-min.js' type='text/javascript'></script>
     <script src='vendor/swaggervel/lib/backbone-min.js' type='text/javascript'></script>
     <script src='vendor/swaggervel/swagger-ui.js' type='text/javascript'></script>
     <script src='vendor/swaggervel/lib/highlight.7.3.pack.js' type='text/javascript'></script>
-    <script src='vendor/swaggervel/lib/jsoneditor.min.js' type='text/javascript'></script>
     <script src='vendor/swaggervel/lib/marked.js' type='text/javascript'></script>
+
     <script src='vendor/swaggervel/lib/swagger-oauth.js' type='text/javascript'></script>
 
     <!-- Some basic translations -->
@@ -31,6 +36,13 @@
     <!-- <script src='lang/en.js' type='text/javascript'></script> -->
 
     <script type="text/javascript">
+
+        function log() {
+            if ('console' in window) {
+                console.log.apply(console, arguments);
+            }
+        }
+
         $(function () {
             var url = window.location.search.match(/url=([^&]+)/);
             if (url && url.length > 1) {
@@ -40,48 +52,61 @@
             }
 
             // Pre load translate...
-            if(window.SwaggerTranslator) {
+            if (window.SwaggerTranslator) {
                 window.SwaggerTranslator.translate();
             }
             window.swaggerUi = new SwaggerUi({
                 url: url,
                 dom_id: "swagger-ui-container",
                 supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
-                onComplete: function(swaggerApi, swaggerUi){
-                    if(typeof initOAuth == "function") {
+                onComplete: function (swaggerApi, swaggerUi) {
+
+                    log("Loaded SwaggerUI");
+                    @if (isset($requestHeaders))
+                    @foreach($requestHeaders as $requestKey => $requestValue)
+                    window.authorizations.add("{!!$requestKey!!}", new ApiKeyAuthorization("{!!$requestKey!!}", "{!!$requestValue!!}", "header"));
+                    @endforeach
+                            @endif
+
+                    if (typeof initOAuth == "function") {
                         initOAuth({
-                            clientId: "your-client-id",
-                            clientSecret: "your-client-secret-if-required",
-                            realm: "your-realms",
-                            appName: "your-app-name",
-                            scopeSeparator: ",",
-                            additionalQueryStringParams: {}
+                            clientId: "{!! $clientId !!}"||"my-client-id",
+                            clientSecret: "{!! $clientSecret !!}"||"_",
+                            realm: "{!! $realm !!}"||"_",
+                            appName: "{!! $appName !!}"||"_",
+                            scopeSeparator: ',',
+                            serviceId: "{{$serviceId}}"||"_",
                         });
+
+                        window.oAuthRedirectUrl = "{{ url('vendor/swaggervel/o2c.html') }}";
+                        $('#clientId').html("{!! $clientId !!}"||"my-client-id");
+                        $('#redirectUrl').html(window.oAuthRedirectUrl);
                     }
 
-                    if(window.SwaggerTranslator) {
+                    if ('{{$serviceId}}')
+                        $('input[name="Service-Id"]').val('{{$serviceId}}');
+
+                    if (window.SwaggerTranslator) {
                         window.SwaggerTranslator.translate();
                     }
 
-                    $('pre code').each(function(i, e) {
+                    $('pre code').each(function (i, e) {
                         hljs.highlightBlock(e)
                     });
 
                     addApiKeyAuthorization();
                 },
-                onFailure: function(data) {
+                onFailure: function (data) {
                     log("Unable to Load SwaggerUI");
                 },
                 docExpansion: "none",
-                jsonEditor: false,
                 apisSorter: "alpha",
-                defaultModelRendering: 'schema',
                 showRequestHeaders: false
             });
 
-            function addApiKeyAuthorization(){
+            function addApiKeyAuthorization() {
                 var key = encodeURIComponent($('#input_apiKey')[0].value);
-                if(key && key.trim() != "") {
+                if (key && key.trim() != "") {
                     var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization("api_key", key, "query");
                     window.swaggerUi.api.clientAuthorizations.add("api_key", apiKeyAuth);
                     log("added key " + key);
@@ -90,19 +115,21 @@
 
             $('#input_apiKey').change(addApiKeyAuthorization);
 
-            // if you have an apiKey you would like to pre-populate on the page for demonstration purposes...
-            /*
-             var apiKey = "myApiKeyXXXX123456789";
-             $('#input_apiKey').val(apiKey);
-             */
+            $('#init-oauth').click(function(){
+                if (typeof initOAuth == "function") {
+                    initOAuth({
+                        clientId: $('#input_clientId').val()||"my-client-id",
+                        clientSecret: $('#input_clientSecret').val()||"_",
+                        realm: $('#input_realm').val()||"_",
+                        appName: $('#input_appName').val()||"_",
+                        scopeSeparator: ',',
+                        serviceId: "{{$serviceId}}"||"_",
+                    });
+                }
+            });
 
             window.swaggerUi.load();
 
-            function log() {
-                if ('console' in window) {
-                    console.log.apply(console, arguments);
-                }
-            }
         });
     </script>
 </head>
@@ -110,7 +137,7 @@
 <body class="swagger-section">
 <div id='header'>
     <div class="swagger-ui-wrap">
-        <a id="logo" href="http://swagger.io">swagger</a>
+        <a id="logo" href="#">api-docs</a>
         <form id='api_selector'>
             <div class='input'><input placeholder="http://example.com/api" id="input_baseUrl" name="baseUrl" type="text"/></div>
             <div class='input'><input placeholder="api_key" id="input_apiKey" name="apiKey" type="text"/></div>
